5557fe7ac76bea4b56ee5a4056b16a97cfee85e3
dishuostec <dishuostec@gmail.com>
2019-10-15 16:01:19 +0800

    use new options

--- src/server.js
+++ src/server.js
@@ -2,19 +2,30 @@ import fs from 'fs';
 import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
-import { build_dir, dev } from '@sapper/internal/manifest-server';
+import { ssr, build_dir, dev } from '@sapper/internal/manifest-server';
 import { generate_mock_api_data } from './mock/generate-mock-api-data.js';
 
 generate_mock_api_data();
 
 const { PORT } = process.env;
 
-polka() // You can also use Express
-	.use(
+const middleware = [
 		compression({ threshold: 0 }),
-		sirv(build_dir, { dev }),
 		sirv('static', { dev }),
-	)
+];
+
+if (ssr) {
+	const sapper = import('@sapper/server');
+	middleware.push(sapper.middleware());
+} else {
+	middleware.push(sirv(build_dir, { dev }));
+}
+
+
+polka() // You can also use Express
+	.use(...middleware)
 	.listen(PORT, err => {
-		if (err) console.log('error', err);
+		if (err) {
+			console.log('error', err);
+		}
 	});
